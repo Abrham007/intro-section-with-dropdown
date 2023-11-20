1bf77cf852897def2ce80c49aa6a7ce46be0cd0d
Abrham007 <abrhamaraya111@gmail.com>
2023-11-20 16:40:05 +0300

    added the nav behaviour

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const menuIcon = document.querySelector(".main-nav__menu-icon");
-
+const mainNav = document.querySelector(".js-main-nav");
+const dropDownLinks = document.querySelectorAll(".js-drop-down-link");
+const dropDowns = document.querySelectorAll(".js-drop-down");
 menuIcon.addEventListener("click", function () {
   menuIcon.classList.toggle("menu-close");
+  mainNav.classList.toggle("display-flex");
 });
+
+for (let i = 0; i < dropDownLinks.length; i++) {
+  dropDownLinks[i].addEventListener("click", function () {
+    dropDowns[i].classList.toggle("display-flex");
+  });
+}
